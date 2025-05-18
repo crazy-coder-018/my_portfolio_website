8209d7c9a42a76b6b88b168953e5a9a3d4080fb5
Tumhara Naam <tumharaemail@example.com>
2025-05-18 19:37:40 +0500

    Header Widget

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -2,7 +2,6 @@
 
 import 'package:flutter/material.dart';
 import 'package:my_portfolio_website/constants/colors.dart';
-import 'package:my_portfolio_website/constants/nav_items.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -20,48 +19,6 @@ class _HomePageState extends State<HomePage> {
         scrollDirection: Axis.vertical,
         children: [
           // Main
-          Container(
-            width: double.maxFinite,
-            margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
-            height: 60,
-            decoration: BoxDecoration(
-              gradient: LinearGradient(
-                colors: [Colors.transparent, CustomColors.bgLight1],
-              ),
-              borderRadius: BorderRadius.circular(100),
-            ),
-            child: Row(
-              children: [
-                // Logo
-                Text(
-                  'C.Coder',
-                  style: TextStyle(
-                    color: CustomColors.yellowSecondary,
-                    fontSize: 22,
-                    fontWeight: FontWeight.w600,
-                  ),
-                ),
-
-                Spacer(),
-
-                for (int i = 0; i < navTitles.length; i++)
-                  Padding(
-                    padding: const EdgeInsets.only(right: 20.0),
-                    child: TextButton(
-                      onPressed: () {},
-                      child: Text(
-                        navTitles[i],
-                        style: TextStyle(
-                          color: CustomColors.whitePrimary,
-                          fontSize: 16,
-                          fontWeight: FontWeight.w500,
-                        ),
-                      ),
-                    ),
-                  ),
-              ],
-            ),
-          ),
 
           // Skills
           Container(
---
+++ lib/widgets/header_desktop.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:my_portfolio_website/constants/colors.dart';
+import 'package:my_portfolio_website/constants/nav_items.dart';
+
+class HeaderDesktop extends StatelessWidget {
+  const HeaderDesktop({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.maxFinite,
+      margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+      height: 60,
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: [Colors.transparent, CustomColors.bgLight1],
+        ),
+        borderRadius: BorderRadius.circular(100),
+      ),
+      child: Row(
+        children: [
+          // Logo
+          Text(
+            'C.Coder',
+            style: TextStyle(
+              color: CustomColors.yellowSecondary,
+              fontSize: 22,
+              fontWeight: FontWeight.w600,
+            ),
+          ),
+
+          Spacer(),
+
+          for (int i = 0; i < navTitles.length; i++)
+            Padding(
+              padding: const EdgeInsets.only(right: 20.0),
+              child: TextButton(
+                onPressed: () {},
+                child: Text(
+                  navTitles[i],
+                  style: TextStyle(
+                    color: CustomColors.whitePrimary,
+                    fontSize: 16,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+              ),
+            ),
+        ],
+      ),
+    );
+  }
+}
