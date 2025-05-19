5fd53598806bb3836a6e94ea2c6e682e092134ad
Tumhara Naam <tumharaemail@example.com>
2025-05-19 12:16:14 +0500

    Hover effect

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -3,6 +3,7 @@
 import 'package:flutter/material.dart';
 import 'package:my_portfolio_website/constants/colors.dart';
 import 'package:my_portfolio_website/constants/nav_items.dart';
+import 'package:my_portfolio_website/widgets/drower_mobile.dart';
 import 'package:my_portfolio_website/widgets/header_mobile.dart';
 
 class HomePage extends StatefulWidget {
@@ -20,60 +21,7 @@ class _HomePageState extends State<HomePage> {
     return Scaffold(
       key: scaffoldKey,
       backgroundColor: CustomColors.scaffoldBG,
-      // Drawer
-      endDrawer: Drawer(
-        backgroundColor: CustomColors.scaffoldBG,
-        child: ListView(
-          children: [
-            // For Closing the Drawer
-            Align(
-              alignment: Alignment.centerRight,
-              child: Padding(
-                padding: const EdgeInsets.only(
-                  top: 10.0,
-                  bottom: 10.0,
-                  left: 20.0,
-                  right: 20.0,
-                ),
-                child: IconButton(
-                  onPressed: () {
-                    scaffoldKey.currentState?.closeEndDrawer();
-                  },
-                  icon: Icon(Icons.close),
-                ),
-              ),
-            ),
-
-            Divider(color: CustomColors.bgLight2),
-            // Menu Bar
-            for (int i = 0; i < navIcons.length; i++)
-              Padding(
-                padding: const EdgeInsets.only(top: 20.0),
-                child: Container(
-                  padding: EdgeInsets.symmetric(horizontal: 4.0),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(16),
-                  ),
-                  child: ListTile(
-                    onTap: () {},
-                    contentPadding: const EdgeInsets.symmetric(
-                      horizontal: 30.0,
-                      vertical: 0.0,
-                    ),
-                    leading: Icon(navIcons[i]),
-                    title: Text(navTitles[i]),
-                    titleTextStyle: TextStyle(
-                      color: CustomColors.whitePrimary,
-                      fontSize: 16,
-                      fontWeight: FontWeight.bold,
-                      letterSpacing: 1.2,
-                    ),
-                  ),
-                ),
-              ),
-          ],
-        ),
-      ),
+      endDrawer: DrowerMobile(),
       body: ListView(
         scrollDirection: Axis.vertical,
         children: [
---
+++ lib/widgets/drower_mobile.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'package:my_portfolio_website/constants/colors.dart';
+import 'package:my_portfolio_website/constants/nav_items.dart';
+
+class DrowerMobile extends StatelessWidget {
+  const DrowerMobile({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      backgroundColor: CustomColors.scaffoldBG,
+      child: ListView(
+        children: [
+          // For Closing the Drawer
+          Align(
+            alignment: Alignment.centerRight,
+            child: Padding(
+              padding: const EdgeInsets.only(
+                top: 20.0,
+                bottom: 10.0,
+                left: 20.0,
+                right: 20.0,
+              ),
+              child: IconButton(
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+                icon: Icon(Icons.close),
+              ),
+            ),
+          ),
+
+          Divider(color: CustomColors.bgLight2),
+
+          // Menu Bar
+          for (int i = 0; i < navIcons.length; i++)
+            Padding(
+              padding: const EdgeInsets.only(top: 10.0),
+              child: Container(
+                padding: EdgeInsets.symmetric(horizontal: 4.0),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(16),
+                ),
+                child: ListTile(
+                  onTap: () {},
+                  hoverColor: CustomColors.bgLight1,
+                  contentPadding: const EdgeInsets.symmetric(
+                    horizontal: 20.0,
+                    vertical: 0.0,
+                  ),
+                  leading: Icon(navIcons[i]),
+                  title: Text(navTitles[i]),
+                  titleTextStyle: TextStyle(
+                    color: CustomColors.whiteSecondary,
+                    fontSize: 16,
+                    fontWeight: FontWeight.w500,
+                    letterSpacing: 1.2,
+                  ),
+                ),
+              ),
+            ),
+        ],
+      ),
+    );
+  }
+}
