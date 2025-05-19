1c957a8926fd1cc6e4317de26835b7d361699ae6
Tumhara Naam <tumharaemail@example.com>
2025-05-19 12:29:45 +0500

    Risponsive touch

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,8 +1,10 @@
 // ignore_for_file: sized_box_for_whitespace
 
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:my_portfolio_website/constants/colors.dart';
 import 'package:my_portfolio_website/widgets/drower_mobile.dart';
+import 'package:my_portfolio_website/widgets/header_desktop.dart';
 import 'package:my_portfolio_website/widgets/header_mobile.dart';
 
 class HomePage extends StatefulWidget {
@@ -17,54 +19,60 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      key: scaffoldKey,
-      backgroundColor: CustomColors.scaffoldBG,
-      endDrawer: DrowerMobile(),
-      body: ListView(
-        scrollDirection: Axis.vertical,
-        children: [
-          // Main
-          // HeaderDesktop(),
-          HeaderMobile(
-            onLogoTap: () {},
-            onMenuTap: () {
-              scaffoldKey.currentState?.openEndDrawer();
-            },
-          ),
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        return Scaffold(
+          key: scaffoldKey,
+          backgroundColor: CustomColors.scaffoldBG,
+          endDrawer: DrowerMobile(),
+          body: ListView(
+            scrollDirection: Axis.vertical,
+            children: [
+              // Main
+              if (constraints.maxWidth >= 600.0)
+                HeaderDesktop()
+              else
+                HeaderMobile(
+                  onLogoTap: () {},
+                  onMenuTap: () {
+                    scaffoldKey.currentState?.openEndDrawer();
+                  },
+                ),
 
-          // Skills
-          Container(
-            width: double.maxFinite,
-            height: 500,
-            color: CustomColors.scaffoldBG,
-            child: Center(child: Text('Skills')),
-          ),
+              // Skills
+              Container(
+                width: double.maxFinite,
+                height: 500,
+                color: CustomColors.scaffoldBG,
+                child: Center(child: Text('Skills')),
+              ),
 
-          // Projects
-          Container(
-            width: double.maxFinite,
-            height: 500,
-            color: CustomColors.bgLight1,
-            child: Center(child: Text('Projects')),
-          ),
+              // Projects
+              Container(
+                width: double.maxFinite,
+                height: 500,
+                color: CustomColors.bgLight1,
+                child: Center(child: Text('Projects')),
+              ),
 
-          // Contact
-          Container(
-            width: double.maxFinite,
-            height: 500,
-            color: CustomColors.bgLight2,
-            child: Center(child: Text('Contact')),
-          ),
+              // Contact
+              Container(
+                width: double.maxFinite,
+                height: 500,
+                color: CustomColors.bgLight2,
+                child: Center(child: Text('Contact')),
+              ),
 
-          // Footer
-          Container(
-            width: double.maxFinite,
-            height: 500,
-            child: Center(child: Text('Footer')),
+              // Footer
+              Container(
+                width: double.maxFinite,
+                height: 500,
+                child: Center(child: Text('Footer')),
+              ),
+            ],
           ),
-        ],
-      ),
+        );
+      },
     );
   }
 }
