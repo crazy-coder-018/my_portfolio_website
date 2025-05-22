5edb770d49185b61fc35d5b17573435d5b88251b
Tumhara Naam <tumharaemail@example.com>
2025-05-23 03:16:27 +0500

    Skills Desktop

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -2,12 +2,12 @@
 
 import 'package:flutter/material.dart';
 import 'package:my_portfolio_website/constants/colors.dart';
-import 'package:my_portfolio_website/constants/skill_items.dart';
 import 'package:my_portfolio_website/widgets/drower_mobile.dart';
 import 'package:my_portfolio_website/widgets/header_desktop.dart';
 import 'package:my_portfolio_website/widgets/header_mobile.dart';
 import 'package:my_portfolio_website/widgets/main_desktop.dart';
 import 'package:my_portfolio_website/widgets/main_desktop_mobile.dart';
+import 'package:my_portfolio_website/widgets/skills_desktop.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -55,6 +55,7 @@ class _HomePageState extends State<HomePage> {
               // Skills Section
               Container(
                 padding: const EdgeInsets.fromLTRB(25, 20, 25, 60),
+                // margin: EdgeInsets.symmetric(horizontal: 20),
                 width: _desktopWidth,
                 color: CustomColors.bgLight1,
                 child: Column(
@@ -62,7 +63,7 @@ class _HomePageState extends State<HomePage> {
                   children: [
                     // Title
                     Text(
-                      'What I can Do',
+                      'What I can Do ?',
                       style: TextStyle(
                         fontSize: 24,
                         fontWeight: FontWeight.bold,
@@ -73,61 +74,7 @@ class _HomePageState extends State<HomePage> {
                     SizedBox(height: 32),
 
                     // Platforms and Skills
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        // Platforms
-                        ConstrainedBox(
-                          constraints: const BoxConstraints(maxWidth: 450),
-                          child: Wrap(
-                            spacing: 5.0,
-                            runSpacing: 5.0,
-                            children: [
-                              for (int i = 0; i < platformItems.length; i++)
-                                Container(
-                                  width: 200,
-                                  decoration: BoxDecoration(
-                                    color: CustomColors.bgLight2,
-                                    borderRadius: BorderRadius.circular(6),
-                                  ),
-                                  child: ListTile(
-                                    contentPadding: EdgeInsets.symmetric(
-                                      vertical: 12,
-                                      horizontal: 12,
-                                    ),
-                                    leading: Image.asset(
-                                      platformItems[i]['img'],
-                                      width: 26,
-                                    ),
-                                    title: Text(platformItems[i]['title']),
-                                  ),
-                                ),
-                            ],
-                          ),
-                        ),
-
-                        SizedBox(width: 50),
-
-                        // Skills
-                        Wrap(
-                          spacing: 5.0,
-                          runSpacing: 5.0,
-                          children: [
-                            for (int i = 0; i < skillItems.length; i++)
-                              Chip(
-                                padding: EdgeInsets.symmetric(
-                                  vertical: 12.0,
-                                  horizontal: 16.0,
-                                ),
-                                label: Text(skillItems[i]['title']),
-                                avatar: Image.asset(skillItems[i]['img']),
-                                backgroundColor: CustomColors.bgLight2,
-                              ),
-                          ],
-                        ),
-                      ],
-                    ),
+                    SkillsDesktop(),
                   ],
                 ),
               ),
