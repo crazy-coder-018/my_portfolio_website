6bc1f92fc5e31cb9fb0f61d50932e368a435cc04
Tumhara Naam <tumharaemail@example.com>
2025-05-23 02:06:46 +0500

    Some Images

---
+++ lib/constants/skill_items.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+List<Map> skillItems = [
+  {"img": "lib/images/flutter.svg", "title": "Flutter"},
+  {"img": "lib/images/dart.svg", "title": "Dart"},
+  {"img": "lib/images/html.png", "title": "HTML5"},
+  {"img": "lib/images/css.png", "title": "CSS3"},
+  {"img": "lib/images/java_script.png", "title": "JS"},
+];
+
+List<Map> platformItems = [
+  {"img": "lib/images/android.svg", "title": "Android Dev"},
+  {"img": "lib/images/web.svg", "title": "Web Dev"},
+  {"img": "lib/images/ios.png", "title": "IOS Dev"},
+  {"img": "lib/images/desktop.png", "title": "Desktop Dev"},
+];
